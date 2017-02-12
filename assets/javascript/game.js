5acedd16ff6ab1b57800e625fdffb54f8629f9b5
Maggie <test@apples-MBP.attlocal.net>
2017-02-11 18:21:37 -0600

    attempt to add counter to the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,36 @@
 
-document.onkeyup = function (event) {
-          console.log("you pressed the " + event.key + " key");
-      }
+// document.onkeyup = function (event) {
+//           console.log("you pressed the " + event.key + " key");
+//       }
 
+// array of words to use in Hangman
+
+// var words = ['cat', 'dog', 'frog', 'bunny'];
+
+// var wordsLength = words.length;
+
+// var wordsDashes = "";
+
+
+// for (var i = 0; i < wordsLength; i++) {
+
+// 	wordsDashes = words[i];
+// 	console.log(wordsDashes);
+// }
+
+// word list
+var words = ["cat", "dog", "cow", "reindeer"];
+
+
+// choose random word from list
+
+var currentWord = words[Math.floor(Math.random() * words.length)];
+
+console.log(currentWord);
+
+// lives
+var lives = 5;
+console.log (lives);
+
+var numberOfLives = document.getElementById("numberOfLives");
+numberOfLives.innerHTML = "You have " + lives + "lives left";
